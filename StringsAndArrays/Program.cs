e56d9981e586d2a8ba2e3f4012b45816d1e96866
IikkaTorkki <103484642+IikkaTorkki@users.noreply.github.com>
2023-10-17 13:51:29 +0300

    Update Program.cs

--- StringsAndArrays/Program.cs
+++ StringsAndArrays/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace StringsAndArrays
+namespace StringsAndArrays
 {
     class Program
     {
+
+        static string ToLovercase(string name)
+        {
+            // Unfortunately no standard library method exists for converting to 'lovercase', so I hope lowercase will do.
+            return name.ToLower();
+        }
+
         // INSTRUCTIONS: Write you task related code answers under each specific comment/insturction
         static void Main(string[] args)
         {
@@ -11,56 +18,79 @@
             string helloWorld = "Hello, World!";
 
             // 1.1 Print the length of the string.
+            Console.WriteLine(helloWorld.Length);
 
             // 1.2 Print the first and the last character of the string.
+            Console.WriteLine($"First: {helloWorld[0]}, last: {helloWorld[helloWorld.Length - 1]}");
 
             // 2. String Manipulation
 
             Console.Write("Please enter your name: ");
             // 2.1 Get the input from and assing it into new string variable called name
+            string name = Console.ReadLine()!;
 
             // 2.2 Print a personalized welcome message including the name.
+            Console.WriteLine($"Welcome {name}, pleased to meet you!");
 
             // 2.3 Convert name variable to uppercase and output the result
+            name = name.ToUpper();
+            Console.WriteLine(name);
 
             // 2.4 Convert name variable to lovercase and output the result
+            name = ToLovercase(name);
+            Console.WriteLine(name);
 
             // 3. String Searching
             // 3.1 Create new boolean variable called containsWorld
+            bool containsWorld;
 
             // 3.2 Check that the helloWorld variable contains the word "World".
             //     Assign the result to the created constainsWorld variable
+            containsWorld = helloWorld.Contains("World");
 
             // 3.3 If the word is found, get the index of the word from helloWorld string
-
+            if (containsWorld)
+            {
+                int indexOfWorld = helloWorld.IndexOf("World");
+            }
 
             // PART 2: Arrays
 
             // 1. Basic Array Operations
             // 1.1 initialize array called numbers containing array of int values
             //     Add these values to the array -> 2 3 5 7 11 
+            int[] numbers = { 2, 3, 5, 7, 11 };
 
             // 1.2 Output the numbers array -> hint use Join
             // Expected outcome: 2, 3, 5, 7, 11
+            Console.WriteLine(string.Join(", ", numbers));
 
             // 1.3 Output the value of the third (3th) item in the numbers array 
             // Expected outcome: 5
-
+            Console.WriteLine(numbers[2]);
 
             // 2. Array Manipulation
             Console.WriteLine("Please enter five names:");
             // 2.1 Initialize new names string array variable that has room for 5 items
             //     The maximum lenght of the string array should be 5
+            string[] names = new string[5];
 
             // 2.2 Ask the user to input new names 5 times
             //     and assing each name to the names string array in the given order.
             //     The first name should be at index 0
             //     Hint: you might want to use some sort of a loop here
 
+            for (int i = 0; i < 5; i++)
+            {
+                names[i] = Console.ReadLine()!;
+            }
+
             // 2.3 Output the array of given names.
+            Console.WriteLine($"The names: {string.Join(", ", names)}");
 
             // 2.4 Reverse the order of the items in the names array and outpu the result
-
+            names = names.Reverse().ToArray();
+            Console.WriteLine($"The names, but reversed: {string.Join(", ", names)}");
 
             // 3. Array Searching
             Console.Write("Please enter a name to search: ");
@@ -68,21 +98,33 @@
             // 3.1 Try to find out the searchName from the Task 2 names string array
             //     by finding out it's index.
             //     Assign the index value to new position integer variable
+            int position = Array.IndexOf(names, searchName);
 
             // 3.2 Check that the index position was found
             //     Hint: What is the result of IndexOf if nothing is found
             // 3.2.1 If position is found output the searchName and the found position
             // 3.2.2 Else output that the searchName was not found
+            if (position != -1)
+            {
+                Console.WriteLine($"{searchName} found at position {position}");
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} was not found");
+            }
 
             // 4. BONUS
 
             // 1. String to Array
             // 1.1 Create new empty words string array
+            string[] words = Array.Empty<string>();
 
             // 1.2 Try to get each word from the helloWorld variable and assing the values to
             //     the created words string array
+            words = helloWorld.Replace(",", "").Replace("!", "").Split(' ').ToArray();
 
             // 1.3 Output the values of the words array
+            Console.WriteLine($"The individual words in helloWorld are: [{string.Join(", ", words)}]");
         }
     }
 }
